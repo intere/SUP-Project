b29b795ad58373af2521c260544aaf94286e772b
Eric Internicola <intere@gmail.com>
2022-07-02 09:54:00 -0600

    Initial Commit

---
+++ SUP-Project/SUP_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUP_ProjectApp.swift
+//  SUP-Project
+//
+//  Created by Eric Internicola on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SUP_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
